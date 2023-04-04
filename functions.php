f5e4a7dcb696362d829b4feeb4e545ec9eae2e23
Larissa Soares Reis <larissasoaresreis@gmail.com>
2023-04-04 15:08:57 -0300

    feat: Requisição no Produto e Transações

---
+++ functions.php
@@ -0,0 +1,8 @@
+<?php
+
+$template_directory = get_template_directory();
+
+require_once($template_directory . 'custom-post-type/transacoes.php');
+require_once($template_directory . 'custom-post-type/produto.php');
+
+?>
